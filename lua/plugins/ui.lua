14d2345f8b9671672bb655feb972717f6a02d520
Eric Rochester <eric.rochester@willowtreeapps.com>
2023-12-05 16:09:20 -0500

    Catppuccin theme

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavor = "mocha",
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
