981c68f00f13607b675fab7328555ff7998427cf
Eric Rochester <erochest@gmail.com>
2024-03-31 10:48:08 -0400

    Plugin updates
    - commented out the symbols-outline extra package because it's giving an
    error message
    - added the copilot extra
    - updates

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,7 +12,8 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.dap.core" },
-    { import = "lazyvim.plugins.extras.editor.symbols-outline" },
+    -- { import = "lazyvim.plugins.extras.editor.symbols-outline" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "lazyvim.plugins.extras.lang.json" },
--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -7,6 +7,7 @@ return {
         "html",
         "javascript",
         "json",
+        "kotlin",
         "lua",
         "markdown",
         "markdown_inline",
