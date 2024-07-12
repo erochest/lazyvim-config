87c12ed85b1874ea493df352bafd536f58da1f76
Eric Rochester <erochest@gmail.com>
2024-07-12 08:04:05 -0400

    if pwsh is on the path, just use that

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,5 +3,9 @@
 -- Add any additional options here
 
 if vim.loop.os_uname().sysname == "Windows_NT" then
-  vim.opt.shell = "C:\\Program Files\\PowerShell\\7\\pwsh.exe"
+  if os.execute("command -v pwsh") == 0 then
+    vim.opt.shell = "pwsh"
+  else
+    vim.opt.shell = "C:\\Program Files\\PowerShell\\7\\pwsh.exe"
+  end
 end
