dbd7fdb211e9effad375f6f3da087191539b061c
Eric Rochester <erochest@gmail.com>
2024-09-02 10:53:32 -0400

    Not using toggleterm

---
+++ lua/plugins/os.lua
@@ -0,0 +1,12 @@
+return {
+  -- {
+  --   "akinsho/toggleterm.nvim",
+  --   opts = {
+  --     -- The default shell to use when `toggleterm` opens
+  --     -- If you're on Windows, you can use `cmd.exe` or `pwsh`
+  --     -- You can also configure some options for the shell
+  --     -- Check `:h toggleterm.nvim` for more information
+  --     shell = "nu",
+  --   },
+  -- },
+}
