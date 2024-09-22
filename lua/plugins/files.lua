01c8452efc26d2f3a5f052c6454cd3ea0268a567
Eric Rochester <erochest@gmail.com>
2024-09-22 17:13:03 -0400

    diff and conflict plugins

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -5,4 +5,18 @@ return {
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
   },
+  { "will133/vim-dirdiff" },
+  {
+    "julienvincent/hunk.nvim",
+    cmd = { "DiffEditor" },
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "echasnovski/mini.icons",
+    },
+    config = function()
+      require("hunk").setup()
+    end,
+  },
+  { "whiteinge/diffconflicts" },
 }
