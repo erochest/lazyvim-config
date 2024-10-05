05ca05f2042360002e52a291824591376d251fd7
Eric Rochester <erochest@gmail.com>
2024-10-04 20:43:38 -0400

    Set nu as my shell

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,9 +3,15 @@
 -- Add any additional options here
 
 if vim.loop.os_uname().sysname == "Windows_NT" then
-  if os.execute("command -v pwsh") == 0 then
-    vim.opt.shell = "pwsh"
-  else
-    vim.opt.shell = "C:\\Program Files\\PowerShell\\7\\pwsh.exe"
-  end
+  vim.opt.shell = "nu"
+  vim.opt.shellcmdflag = "-c"
+  vim.opt.shellredir = "2>&1 | save --raw %s"
+  vim.opt.shellpipe = "2>&1 | save --raw %s"
+  vim.opt.shellquote = "'"
+  vim.opt.shellxquote = ""
+  -- if os.execute("command -v pwsh") == 0 then
+  --   vim.opt.shell = "pwsh"
+  -- else
+  --   vim.opt.shell = "C:\\Program Files\\PowerShell\\7\\pwsh.exe"
+  -- end
 end
