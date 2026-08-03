fbdd20b81a641f780d0b31c4245efe615960c8e6
Eric Rochester <erochest@gmail.com>
2026-08-03 07:53:03 -0400

    jujutsu

--- lua/plugins/jj.lua
+++ lua/plugins/jj.lua
@@ -1,24 +1,26 @@
 return {
-  {
-    "jceb/jiejie.nvim",
-    dependencies = {
-      {
-        "folke/which-key.nvim",
-        optional = true,
-        opts = {
-          spec = {
-            { "<leader>j", group = "Jujutsu" },
-          },
-        },
-      },
-    },
-    keys = {
-      { "<leader>jl", "<cmd>JJ<cr>", desc = "Show the log" },
-      { "<leader>jo", "<cmd>J oplog<cr>", desc = "Show the operation log" },
-      { "<leader>jq", "<cmd>JcLog<cr>", desc = "Quickfix log" },
-    },
-  },
+  -- {
+  --   "jceb/jiejie.nvim",
+  --   dependencies = {
+  --     {
+  --       "folke/which-key.nvim",
+  --       optional = true,
+  --       opts = {
+  --         spec = {
+  --           { "<leader>j", group = "Jujutsu" },
+  --         },
+  --       },
+  --     },
+  --   },
+  --   keys = {
+  --     { "<leader>jl", "<cmd>JJ<cr>", desc = "Show the log" },
+  --     { "<leader>jo", "<cmd>J oplog<cr>", desc = "Show the operation log" },
+  --     { "<leader>jq", "<cmd>JcLog<cr>", desc = "Quickfix log" },
+  --   },
+  -- },
+
   "evanphx/jjsigns.nvim",
+
   {
     "julienvincent/hunk.nvim",
     cmd = { "DiffEditor" },
@@ -31,8 +33,11 @@ return {
       require("hunk").setup()
     end,
   },
+
   { "rafikdraoui/jj-diffconflicts" },
+
   { "avm99963/vim-jjdescription" },
+
   {
     "willothy/flatten.nvim",
     config = true,
@@ -41,5 +46,21 @@ return {
     -- Ensure that it runs first to minimize delay when opening file from terminal
     lazy = false,
     priority = 1001,
+  },
+
+  {
+    "mistweaverco/jujutsu.nvim",
+    lazy = true,
+    dependencies = { "sindrets/diffview.nvim", "ibhagwan/fzf-lua" },
+    keys = {
+      {
+        "<leader>gg",
+        function()
+          require("jujutsu").open()
+        end,
+        desc = "Jujutsu",
+      },
+    },
+    opts = {}, -- passed to setup()
   },
 }
