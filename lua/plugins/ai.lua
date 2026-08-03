93cc4f588e51361ad9b6927dc4eb239d3c19c160
Eric Rochester <erochest@gmail.com>
2026-08-03 07:54:40 -0400

    pi2

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -103,4 +103,19 @@ return {
       { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
     },
   },
+
+  {
+    "zgs225/pi2.nvim",
+
+    -- render-markdown.nvim powers the default chat-history renderer
+    -- (render.engine = "render-markdown"); img-clip.nvim is optional and
+    -- required only for `:PiPasteImage` (clipboard image paste).
+    dependencies = {
+      "MeanderingProgrammer/render-markdown.nvim",
+      "HakonHarnes/img-clip.nvim",
+    },
+
+    -- if you're fine with defaults:
+    config = true,
+  },
 }
